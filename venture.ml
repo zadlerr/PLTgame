e0384428621fe4bc07f6afce0d2da7241c7a1c61
jssands <js4597@columbia.edu>
2017-03-24 16:15:39 -0400

    driver file added - not yet tested

--- venture.ml
+++ venture.ml
@@ -1,10 +1,20 @@
-open Ast
+(* Top-level of the MicroC compiler: scan & parse the input,
+   check the resulting AST, generate LLVM IR, and dump the module *)
 
-let eval = function
-    Lit(x) -> x
+type action = Ast | LLVM_IR | Compile
 
-let _ = 
-    let lexbuf = Lexing.from_channel stdin in
-    let program_object = Parser.program Scanner.token lexbuf in
-    let int2print = eval program_object in
-    print_endline(string_of_int int2print)
+let _ =
+  let action = if Array.length Sys.argv > 1 then
+    List.assoc Sys.argv.(1) [ ("-a", Ast);	(* Print the AST only *)
+			      ("-l", LLVM_IR);  (* Generate LLVM, don't check *)
+			      ("-c", Compile) ] (* Generate, check LLVM IR *)
+  else Compile in
+  let lexbuf = Lexing.from_channel stdin in
+  let ast = Parser.program Scanner.token lexbuf in
+  Semant.check ast;
+  match action with
+    Ast -> print_string (Ast.string_of_program ast)
+  | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
+  | Compile -> let m = Codegen.translate ast in
+    Llvm_analysis.assert_valid_module m;
+    print_string (Llvm.string_of_llmodule m)
