935e6da3b3950743d034e9f3787189cf1ff2ec8d
zpa2001 <zpa2001@columbia.edu>
2017-04-02 18:07:42 -0400

    init commit of semant.ml

---
+++ semant.ml
@@ -0,0 +1,38 @@
+open Ast
+
+module StringMap = Map.Make(String)
+
+let check (globals, functions) = 
+
+  (* same as microC, sorts a given list to see if there are multiple entires of      same keyword *)
+  let report_duplicates exceptf list = 
+    let rec helper = function 
+	 n1 :: n2 :: _ when n1 = n2 -> raise(Failure (exceptf n1))
+	| _ :: t -> helper t
+	| [] -> ()
+    in helper (List.sort compare list)
+  in 
+
+ (* used for checking to see if a variable has type void, returns unit if not*) 
+  let check_not_void exceptf = function 
+	(Void, n) -> raise (Failure (exceptf n))
+    | _ -> ()
+  in 
+
+  let check_assign lvaluet rvaluet err =
+    (* types are equal*) 
+    (* implement op == *)
+    if lvaluet == rvaluet then lvaluet else raise err 
+  in 
+
+  (* CHECKING GLOBAL VARIABLES *)
+
+  List.iter (check_not_void (fun n -> "illegal void global " ^ n)) globals;
+
+  report_duplicate (fun n -> "duplicate global " ^ n) (List.map snd globals);
+
+  (**** Checking Functions ****)
+  (* check for "primitive (built in functions) *)
+  (*if List.mem "print" (List.map*)
+   
+
