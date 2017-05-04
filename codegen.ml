6efdf6fc25c8f2a8b378d41b972bdb9f1ec6ee88
nsahra14 <nsahra14@gmail.com>
2017-05-04 18:26:55 -0400

    cleaned up codegen of comments

--- codegen.ml
+++ codegen.ml
@@ -1,6 +1,5 @@
 module L = Llvm
-(*open Ast
-*)
+(*open Ast*)
 module StringMap = Map.Make(String)
 
 let translate (globals, functions) =
@@ -28,13 +27,13 @@ let translate (globals, functions) =
 	    List.fold_left global_var StringMap.empty globals
 	      in
 
-  (*Declare printf(), which the print built-in function will call *)
+  (*Declaration of printf(), which the print built-in function will call *)
   let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] 
     in
     let printf_func = L.declare_function "printf" printf_t the_module 
       in
 
-  (*Declare scompare for strings from external c file*)
+  (*Declare other builtin fns included in stdlib c file*)
   let scompare_formals = [(Ast.String, "s1"); (Ast.String, "s2")] in
   let sformal_types = Array.of_list (List.map (fun (t,_) -> ltype_of_typ t) scompare_formals) in
   let scompare_t = L.function_type i32_t sformal_types in
@@ -46,8 +45,6 @@ let translate (globals, functions) =
   let input_t = L.function_type str_t [| |] in
   let input_func = L.declare_function "input" input_t the_module in
 
-
-  (* Ignoring printbig for now too *)
 
   (* Define each function (arguments and return type ) so we can call it  *)
   let function_decls = 
@@ -61,22 +58,17 @@ let translate (globals, functions) =
           List.fold_left function_decl StringMap.empty functions 
 	    in
 
-
-(* need to build function bodies *)
-(* by building expressions *)
-(* by building statements *)
+(* function bodies built by building expressions and statements *)
  
 (* Fill in the body of the given function *)
   let build_function_body fdecl =   (* function to build body of function *)
     let (the_function, _) = StringMap.find fdecl.Ast.fname function_decls 
-      in (* find function declaration from map *)
+      in
       let builder = L.builder_at_end context (L.entry_block the_function) 
-        in (* builds at end of basic block. A basic block is simply a container of instructions that execute sequentially*)
-        let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder
-          in 
-	let str_format_str = L.build_global_stringptr "%s\n" "fmt" builder in(* llvm int to string. let's see where it's used *)
-	let char_format_str = L.build_global_stringptr "%c" "fmt" builder
-        in 
+       in 
+        let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder in 
+	let str_format_str = L.build_global_stringptr "%s\n" "fmt" builder in
+	let char_format_str = L.build_global_stringptr "%c" "fmt" builder in 
 
  
     let local_vars = 
@@ -94,7 +86,6 @@ let translate (globals, functions) =
 
     let formals = List.fold_left2 add_formal StringMap.empty fdecl.Ast.formals (Array.to_list (L.params the_function))
       in
-      (* line add locals to local_vars. Formals already in local_vars *)
         List.fold_left add_local formals fdecl.Ast.locals
         in
 
@@ -130,7 +121,6 @@ let translate (globals, functions) =
       | Ast.Char_Literal c -> L.const_int i8_t (int_of_char c)
       | Ast.BoolLit b -> L.const_int i1_t (if b then 1 else 0)
       | Ast.Noexpr -> L.const_int i32_t 0
-      (* for formals lookup need Id. Need Call for 'printf' *)
       | Ast.Id s -> L.build_load (lookup s) s builder
       | Ast.Assign (s, e) -> let e' = expr builder e in 
 		ignore (L.build_store e' (lookup s) builder); e'
@@ -176,15 +166,11 @@ let translate (globals, functions) =
         
 	in
 
-    (* Invoke "f builder" if the current block doesn't already
-       have a terminal (e.g., a branch). *)
     let add_terminal builder f =
       match L.block_terminator (L.insertion_block builder) with
       	  Some _ -> ()
         | None -> ignore (f builder) in
 	
-    (* Build the code for the given statement; return the builder for
-       the statement's successor *)
     let rec stmt builder = function
 	Ast.Block sl -> List.fold_left stmt builder sl
       | Ast.Expr e -> ignore (expr builder e); builder
