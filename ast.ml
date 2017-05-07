e9b570d03ae12c5f60673b01158ad09b551a3124
Zachary Adler <zpa2001@columbia.edu>
2017-05-06 23:12:38 -0400

    adding new ast.ml, ast.mli is deleted, semant and venture all updated

---
+++ ast.ml
@@ -0,0 +1,106 @@
+type op = Add | Sub | Mult | Div | Mod | Equal | Neq | Lessthan | Greaterthan | Leq | Geq | And | Or
+
+ type uop = Neg | Not
+
+type typ = Int | String | Char | Bool | Void 
+
+type bind = typ * string
+
+type expr =
+	  Int_Literal of int
+	| String_Lit of string
+	| Char_Literal of char
+	| BoolLit of bool
+	| Id of string
+	| Binop of expr * op * expr
+	| Unop of uop * expr
+	| Assign of string * expr
+	| Call of string * expr list
+	| Noexpr
+
+
+type stmt =
+	  Block of stmt list
+	| Expr of expr
+	| If of expr * stmt * stmt
+	| While of expr * stmt
+	| Return of expr  
+
+
+type func_decl = {
+    typ : typ;
+    fname: string;
+    formals : bind list; 
+    locals : bind list;
+    body : stmt list;
+}
+ 
+type program = bind list * func_decl list
+
+(* Pretty-printing functions *)
+
+let string_of_op = function
+    Add -> "+"
+  | Sub -> "-"
+  | Mult -> "*"
+  | Div -> "/"
+  | Equal -> "=="
+  | Neq -> "!="
+  | Lessthan -> "<"
+  | Leq -> "<="
+  | Greaterthan -> ">"
+  | Geq -> ">="
+  | And -> "&&"
+  | Mod -> "%"
+  | Or -> "||"
+
+let string_of_uop = function
+    Neg -> "-"
+  | Not -> "!"
+
+let rec string_of_expr = function
+    Int_Literal(l) -> string_of_int l
+  | BoolLit(true) -> "true"
+  | BoolLit(false) -> "false"
+  | String_Lit l -> l 
+  | Char_Literal(l) -> String.make 1 l 
+  | Id(s) -> s
+  | Binop(e1, o, e2) ->
+      string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
+  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
+  | Call(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  | Noexpr -> ""
+
+let rec string_of_stmt = function
+    Block(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n";
+  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
+  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
+  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
+      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+
+let string_of_typ = function
+    Int -> "int"
+  | Bool -> "bool"
+  | String -> "string"
+  | Char -> "char"
+  | Void -> "void"
+
+let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+
+let string_of_fdecl fdecl =
+  string_of_typ fdecl.typ ^ " " ^
+  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
+
+let string_of_program (vars, funcs) =
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_fdecl funcs)
+
