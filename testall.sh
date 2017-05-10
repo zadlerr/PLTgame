4a623fbe93d0d128228dd91dbc80a01c8b619578
nsahra14 <nsahra14@gmail.com>
2017-05-09 20:12:01 -0400

    cleanup of testall script

--- testall.sh
+++ testall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Regression testing script for MicroC
+# testing script for VENTURE
 # Step through a list of files
 #  Compile, run, and check the output of each expected-to-work test
 #  Compile and check the error of each expected-to-fail test
